4b159da4430996fcd7f01a2f699459894a8bba87
jack_kerouac <jack_kerouac>
2008-12-02 21:41:52 +0000

    - minor changes

--- deus/core/puddle/src/main/java/inf6/promed/deus/TestClass.java
+++ deus/core/puddle/src/main/java/inf6/promed/deus/TestClass.java
@@ -14,31 +14,28 @@ public class TestClass {
 	 * Dependency Injection:
 	 * 
 	 * @Required
-	 * 
 	 * @Autowired
-	 * 
 	 * @Resource(name="beanNametoInject") (JSR-250)
 	 * 
 	 * Initialization/Destruction callbacks:
 	 * 
 	 * @PostConstruct
-	 * 
 	 * @PreDestroy
 	 * 
 	 * Managed Components:
 	 * 
 	 * @Component
-	 * 
-	 * @Controller
-	 * 
-	 * @Service
-	 * 
-	 * @Repository
+	 * 	@Controller
+	 * 	@Service
+	 * 	@Repository
 	 */
 
 	@PostConstruct
 	public void start() {
 		System.out.println("HEEEEELLLOOO WORLD");
+
+		System.out.println(2);
+
 	}
 
 	@PreDestroy
