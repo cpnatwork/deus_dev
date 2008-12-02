4bb58581aa5bf916f8274a0b6d722d4bd1196faf
jack_kerouac <jack_kerouac>
2008-12-02 17:07:15 +0000

    - fixed issues with context:component-scan

--- deus/core/puddle/src/main/java/inf6/promed/deus/TestClass.java
+++ deus/core/puddle/src/main/java/inf6/promed/deus/TestClass.java
@@ -1,37 +1,49 @@
 package inf6.promed.deus;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.stereotype.Component;
+
 @Component
 public class TestClass {
-	
+
 	/*
 	 * Spring Annotations:
 	 * 
 	 * Dependency Injection:
 	 * 
 	 * @Required
+	 * 
 	 * @Autowired
-	 * @Resource(name="beanNametoInject")  (JSR-250)
+	 * 
+	 * @Resource(name="beanNametoInject") (JSR-250)
 	 * 
 	 * Initialization/Destruction callbacks:
 	 * 
 	 * @PostConstruct
+	 * 
 	 * @PreDestroy
 	 * 
 	 * Managed Components:
-	 * @Component
-	 * 	@Controller
-	 *  @Service
-	 *  @Repository
 	 * 
+	 * @Component
+	 * 
+	 * @Controller
+	 * 
+	 * @Service
+	 * 
+	 * @Repository
 	 */
-	
 
+	@PostConstruct
 	public void start() {
 		System.out.println("HEEEEELLLOOO WORLD");
 	}
-	
+
+	@PreDestroy
 	public void stop() {
 		System.out.println("GOODBYE WORLD");
-	}	
-	
+	}
+
 }
