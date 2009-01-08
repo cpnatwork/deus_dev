a261f808fa8d01cbbe2fe7676f3c0346902faac5
jack_kerouac <jack_kerouac>
2009-01-08 17:01:57 +0000

    - added getter and setter for DatePlace

--- deus/core/puddle/src/main/java/deus/model/dossier/proj/party/common/DatePlace.java
+++ deus/core/puddle/src/main/java/deus/model/dossier/proj/party/common/DatePlace.java
@@ -6,5 +6,25 @@ public class DatePlace {
 
 	private Date date;
 	private String place;
-	
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	public String getPlace() {
+		return place;
+	}
+
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
 }
