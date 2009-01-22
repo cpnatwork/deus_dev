71472a1bba98bb1830b7fd6c3e744b03f700fbbb
jack_kerouac <jack_kerouac>
2009-01-22 14:24:40 +0000

    - removed last template

--- deus/core/puddle/src/main/java/deus/core/subscriber/impl/RemoteCalledSubscriberImpl.java
+++ deus/core/puddle/src/main/java/deus/core/subscriber/impl/RemoteCalledSubscriberImpl.java
@@ -2,12 +2,10 @@ package deus.core.subscriber.impl;
 
 
 import deus.core.subscriber.RemoteCalledSubscriber;
-import deus.model.depository.generic.DistributedInformationFolder;
 import deus.model.dossier.generic.ForeignInformationFile;
 import deus.model.sub.PublisherMetadata;
 
-public class RemoteCalledSubscriberImpl<DifType extends DistributedInformationFolder> implements
-		RemoteCalledSubscriber {
+public class RemoteCalledSubscriberImpl implements RemoteCalledSubscriber {
 
 	public RemoteCalledSubscriberImpl() {
 		super();
