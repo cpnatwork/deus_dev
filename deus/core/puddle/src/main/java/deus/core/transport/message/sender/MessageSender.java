2f55ba761a701764b5730162b950fb84697ece87
jack_kerouac <jack_kerouac>
2009-02-01 11:09:22 +0000

    - removed transport package from core-puddle

--- deus/core/puddle/src/main/java/deus/core/transport/message/sender/MessageSender.java
+++
@@ -1,9 +0,0 @@
-package deus.core.transport.message.sender;
-
-import deus.core.transport.message.TransportMessage;
-
-public interface MessageSender {
-
-	public void send(TransportMessage command);
-	
-}
--- deus/core/puddle/src/main/java/deus/core/transport/observers/PasswordLookup.java
+++
@@ -1,9 +0,0 @@
-package deus.core.transport.observers;
-
-import deus.core.transport.id.TransportId;
-
-public interface PasswordLookup {
-
-	public String getPassword(TransportId transportId);
-
-}
