e447fca011739e984008f79bd787b314575c68ef
jack_kerouac <jack_kerouac>
2009-02-05 07:17:24 +0000

    - added lib-mvn-ext - added SMACK jar files to lib-mvn-ext - added install script

---
+++ lib-mvn-ext/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mvn install:install-file -Dfile=smack-3.1.0.jar -DartifactId=smack -DgroupId=jivesoftware -Dversion=3.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=smackx-3.1.0.jar -DartifactId=smackx -DgroupId=jivesoftware -Dversion=3.1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=smackx-debug-3.1.0.jar -DartifactId=smackx-debug -DgroupId=jivesoftware -Dversion=3.1.0 -Dpackaging=jar
