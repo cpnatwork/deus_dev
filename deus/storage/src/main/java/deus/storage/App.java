35cc8269220715faa7e908145c49a925b30b9aa6
cpnatwork <cpnatwork@googlemail.com>
2009-02-17 20:10:05 +0000

    - move Clay file

---
+++ deus/storage/src/main/java/deus/storage/App.java
@@ -0,0 +1,13 @@
+package deus.storage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
