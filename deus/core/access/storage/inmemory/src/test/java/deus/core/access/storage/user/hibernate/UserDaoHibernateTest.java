0e8be50f67fdd14696830e7b566cd95ccf4f5d40
jack_kerouac <jack_kerouac>
2009-02-20 13:59:01 +0000

    - split up storage to three bundles

---
+++ deus/core/access/storage/inmemory/src/test/java/deus/core/access/storage/user/hibernate/UserDaoHibernateTest.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package deus.core.access.storage.user.hibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import deus.core.access.storage.user.api.UserDao;
+import deus.model.user.id.UserId;
+import deus.model.user.id.UserUrl;
+
+/**
+ * @author cpn
+ * 
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "/deus/context.xml", "/deus/storage/hibernate.xml" })
+public class UserDaoHibernateTest {
+
+	@Autowired
+	private UserDao sut;
+
+
+	@Test
+	public void testCreate() {
+//		UserId userId = new UserUrl("testuser_UserDaoTest", "testhostname_UserDaoTest");
+//		sut.addNewEntity(userId);
+	}
+
+
+	@Test
+	public void testGet() {
+//		sut.getByNaturalId("testuser_UserDaoTest");
+	}
+
+
+	@Test
+	public void testDelete() {
+//		sut.deleteByNaturalId("testuser_UserDaoTest");
+	}
+}
