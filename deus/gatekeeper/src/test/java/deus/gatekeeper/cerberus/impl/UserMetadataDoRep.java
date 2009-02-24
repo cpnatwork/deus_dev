708091173a79fcb9a148615b6930fca7c59eb02f
jack_kerouac <jack_kerouac>
2009-02-24 21:12:59 +0000

    - added DoRepStub

---
+++ deus/gatekeeper/src/test/java/deus/gatekeeper/cerberus/impl/UserMetadataDoRep.java
@@ -0,0 +1,21 @@
+package deus.gatekeeper.cerberus.impl;
+
+import deus.model.user.UserMetadata;
+import deus.model.user.id.UserId;
+
+public class UserMetadataDoRep implements deus.core.access.storage.api.user.UserMetadataDoRep {
+
+	@Override
+	public void addNewEntity(UserId userId, UserMetadata userMetadata) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public UserMetadata getByNaturalId(UserId userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
