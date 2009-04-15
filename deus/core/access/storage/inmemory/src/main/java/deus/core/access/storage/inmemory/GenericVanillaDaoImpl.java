262a4354747085ae1a1897b915bf4e823f3b3575
jack_kerouac <jack_kerouac>
2009-04-15 16:40:41 +0000

    - removed unnecessary barker package - formatted dao

--- deus/core/access/storage/inmemory/src/main/java/deus/core/access/storage/inmemory/GenericVanillaDaoImpl.java
+++ deus/core/access/storage/inmemory/src/main/java/deus/core/access/storage/inmemory/GenericVanillaDaoImpl.java
@@ -8,42 +8,46 @@ import java.util.HashMap;
  * 
  * @author cpn
  */
-public class GenericVanillaDaoImpl<EntityT, NaturalIdT extends Serializable> implements GenericVanillaDao<EntityT, NaturalIdT>{
+public class GenericVanillaDaoImpl<EntityT, NaturalIdT extends Serializable> implements
+		GenericVanillaDao<EntityT, NaturalIdT> {
 
 	protected HashMap<NaturalIdT, EntityT> storage = new HashMap<NaturalIdT, EntityT>();
-	
+
+
 	protected HashMap<NaturalIdT, EntityT> getStorage() {
 		return storage;
 	}
+
 
 	@Override
 	public void addNewEntity(NaturalIdT naturalId, EntityT entity) {
 		getStorage().put(naturalId, entity);
 	}
 
+
 	@Override
 	public EntityT getByNaturalId(NaturalIdT naturalId) {
 		return getStorage().get(naturalId);
 	}
 
+
 	@Override
 	public void updateEntity(NaturalIdT naturalId, EntityT entity) {
 		getStorage().put(naturalId, entity);
 	}
-	
+
+
 	@Override
 	public void deleteByNaturalId(NaturalIdT naturalId) {
-		assert(getStorage().containsKey(naturalId));
-		getStorage().remove(naturalId);		
+		assert (getStorage().containsKey(naturalId));
+		getStorage().remove(naturalId);
 	}
+
 
 	@Override
 	public boolean existsByNaturalId(NaturalIdT naturalId) {
 		return getStorage().containsKey(naturalId);
 	}
-
-
-
 
 
 }
