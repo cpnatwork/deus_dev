f12bb48ddeb1bc35e62203a90be9041e0d9fe8b7
jack_kerouac <jack_kerouac>
2009-04-15 19:23:03 +0000

    added tests for barker

---
+++ deus/core/soul/hci/src/test/java/deus/common/EasyMockFactoryBean.java
@@ -0,0 +1,47 @@
+package deus.common;
+
+import org.easymock.EasyMock;
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * This is needed for autowiring mocks, that are created by EasyMock. Spring uses the return type of factory methods for
+ * autowiring the beans that are created by these methods. Since <code>Easymock.createMock(Class)</code> returns an
+ * Object, Spring fails to autowire Mocks created by this method.
+ * 
+ * Thus, this class is used as a helper, since there is the method <code>getObjectType</code>, that explicitly specifies
+ * the return type.
+ * 
+ * See more on this issue at http://javadevelopmentforthemasses.blogspot.com/2008/07/mocking-spring-tests.html.
+ * 
+ * @author Florian Rampp (Florian.Rampp@informatik.stud.uni-erlangen.de)
+ * 
+ * @param <T>
+ */
+public class EasyMockFactoryBean<T> implements FactoryBean {
+
+	private Class<T> type;// the created object type
+
+
+	public void setType(final Class<T> type) {
+		this.type = type;
+	}
+
+
+	@Override
+	public Object getObject() throws Exception {
+		return EasyMock.createMock(type);
+	}
+
+
+	@Override
+	public Class<T> getObjectType() {
+		return type;
+	}
+
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+	
+}
---
+++ deus/core/soul/hci/src/test/java/deus/core/soul/hci/barker/BarkerExportedToClientTest.java
@@ -0,0 +1,116 @@
+package deus.core.soul.hci.barker;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.createMock;
+import static org.easymock.classextension.EasyMock.createNiceMock;
+import static org.easymock.classextension.EasyMock.replay;
+import static org.easymock.classextension.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.easymock.classextension.EasyMock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import deus.core.access.storage.api.hci.attention.AttentionElementDao;
+import deus.core.access.storage.api.hci.attention.AttentionListDao;
+import deus.model.common.user.id.UserId;
+import deus.model.hci.attention.AttentionElement;
+import deus.model.hci.attention.AttentionList;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/all.xml" })
+public class BarkerExportedToClientTest {
+
+	@Autowired
+	private BarkerExportedToClient barkerSUT;
+
+	@Autowired
+	private AttentionElementDao attentionElementDaoMock;
+
+	@Autowired
+	private AttentionListDao attentionListDaoMock;
+
+
+	private UserId userIdFix;
+	private AttentionElement attentionElementFix;
+
+
+	@Before
+	public void setUp() throws Exception {
+		userIdFix = createMock(UserId.class);
+		attentionElementFix = createNiceMock(AttentionElement.class);
+	}
+
+
+	@After
+	public void tearDown() throws Exception {
+		org.easymock.EasyMock.reset(attentionElementDaoMock, attentionListDaoMock);
+	}
+
+
+	@Test
+	public void testNoticeAttentionElement_HappyPath() {
+		attentionElementFix.setNoticed(true);
+		replay(attentionElementFix);
+
+		attentionElementDaoMock.updateEntity(userIdFix, attentionElementFix);
+		replay(attentionElementDaoMock);
+
+		barkerSUT.noticeAttentionElement(userIdFix, attentionElementFix);
+
+		verify(attentionElementFix);
+		verify(attentionElementDaoMock);
+	}
+
+
+	@Test
+	public void testNoticeAttentionElement_AeAlreadyNoticed() {
+		expect(attentionElementFix.isNoticed()).andReturn(true);
+		replay(attentionElementFix);
+
+		try {
+			barkerSUT.noticeAttentionElement(userIdFix, attentionElementFix);
+			fail();
+		}
+		catch (BarkerRuntimeException e) {
+			// expected
+		}
+
+		verify(attentionElementFix);
+	}
+
+
+	@Test
+	public void testGetUnnoticedAttentionList() {
+		AttentionList expectedRetVal = null;
+
+		expect(attentionListDaoMock.getUnnoticedAttentionList(userIdFix)).andReturn(expectedRetVal);
+		replay(attentionListDaoMock);
+
+		AttentionList sutRetVal = barkerSUT.getUnnoticedAttentionList(userIdFix);
+		assertEquals(expectedRetVal, sutRetVal);
+
+		verify(attentionListDaoMock);
+	}
+
+
+	@Test
+	public void testGetNoticedAttentionList() {
+		AttentionList expectedRetVal = EasyMock.createNiceMock(AttentionList.class);
+
+		expect(attentionListDaoMock.getNoticedAttentionList(userIdFix)).andReturn(expectedRetVal);
+		replay(attentionListDaoMock);
+
+		AttentionList sutRetVal = barkerSUT.getNoticedAttentionList(userIdFix);
+		assertEquals(expectedRetVal, sutRetVal);
+
+		verify(attentionListDaoMock);
+	}
+
+}
