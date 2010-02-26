2ead29d76cfa18a51ea06a2a117a69cf86c2e6ac
jack_kerouac <jack_kerouac>
2010-02-26 14:31:43 +0000

    - adjusted pifgoverning subsystem

--- deus/core/soul/pifgoverning/src/main/java/deus/core/soul/contribution/Contributor.java
+++
@@ -1,9 +0,0 @@
-package deus.core.soul.contribution;
-
-import deus.core.access.transfer.core.receiving.soulcallback.contribution.ContributorExportedToPeers;
-
-
-public interface Contributor extends ContributorExportedToClient, ContributorExportedToPeers {
-
-	
-}
